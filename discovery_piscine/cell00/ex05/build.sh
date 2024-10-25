42e26d77c4850e5e42e4c361e1df3776b57909a5
mrkdxb <hamouzou621@gmail.com>
2024-10-25 16:23:13 +0200

    Add files via upload

---
+++ discovery_piscine/cell00/ex05/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Vérifie si au moins un argument est fourni
+if [ "$#" -eq 0 ]; then
+    echo "Usage: $0 ex nom_du_dossier1 nom_du_dossier2 ..."
+    exit 1
+fi
+
+# Crée chaque dossier avec 'ex' au début du nom
+for dir; do
+    mkdir ex$dir
+done
+
+
